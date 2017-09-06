6bfcba9bf4e53088d216119b6e43f35811f0f8a7
Justin Tooley <jstooley823@gmail.com>
2017-09-06 05:18:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,11 +16,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  longer = false
-  array.collect do |word|
-    if word.length <  4
-       longer = true
-    end
+  bool_arrray = []
+  array.each_with_index do |word, i|
+    bool_array[i] = word < 4
   end
   longer
 end
