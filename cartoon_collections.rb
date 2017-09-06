dde315b0320e20f2a456747fdfcc32e7585678e1
Justin Tooley <jstooley823@gmail.com>
2017-09-06 05:11:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,12 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.collect do |word|
+    if word.length <  4
+      return true
+    else
+      false
+  end
 end
 
 def find_the_cheese# code an argument here
